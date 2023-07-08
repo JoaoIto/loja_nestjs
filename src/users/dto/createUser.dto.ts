66a1b22e14e7159edbf7a848643fee1bd90bab65
João <joaovictorpfr@gmail.com>
2023-07-08 10:30:20 -0300

    validation user dto

--- src/users/dto/createUser.dto.ts
+++ src/users/dto/createUser.dto.ts
@@ -2,14 +2,21 @@ import { IsString, IsEmail, MinLength, IsNotEmpty } from 'class-validator';
 
 export class CreateUserDTO {
   @IsString()
-  @IsNotEmpty()
+  @IsNotEmpty({ message: 'O nome não pode estar vazio!' })
   nome: string;
 
-  @IsEmail()
-  @IsNotEmpty()
+  @IsEmail(undefined, {
+    message:
+      'O email é inválido! Verifique se esqueceu alguma informação ou ocorreu erro de digitação!',
+  })
+  @IsNotEmpty({
+    message: 'O email não pode estar vazio!',
+  })
   email: string;
 
-  @MinLength(6)
-  @IsNotEmpty()
+  @MinLength(6, { message: 'A senha deve ter no mínimo 6 caracteres!' })
+  @IsNotEmpty({
+    message: 'a senha não pode estar vazia!',
+  })
   senha: string;
 }
